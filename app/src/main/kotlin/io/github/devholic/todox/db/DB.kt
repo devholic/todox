837a6e54456f8954d528d249313490646dbc58fe
devholic <devholic@plusquare.com>
2016-09-17 21:35:11 +0900

    db: add `getInstance` to DB
    BriteDatabase를 리턴하는 `getInstance` 메소드를 추가했습니다.

--- app/src/main/kotlin/io/github/devholic/todox/db/DB.kt
+++ app/src/main/kotlin/io/github/devholic/todox/db/DB.kt
@@ -1,8 +1,26 @@
 package io.github.devholic.todox.db
 
+import android.content.Context
 import android.database.Cursor
+import com.squareup.sqlbrite.BriteDatabase
+import com.squareup.sqlbrite.SqlBrite
+import rx.schedulers.Schedulers
 
 object DB {
+
+    private val sqlbrite: SqlBrite by lazy { SqlBrite.create() }
+
+    private var database: BriteDatabase? = null
+
+    fun getInstance(context: Context): BriteDatabase {
+        if (database == null) {
+
+            val helper = DBHelper(context.applicationContext)
+
+            database = sqlbrite.wrapDatabaseHelper(helper, Schedulers.io())
+        }
+        return database!!
+    }
 
     fun getInt(cursor: Cursor, column: String): Int {
         return cursor.getInt(cursor.getColumnIndexOrThrow(column))
